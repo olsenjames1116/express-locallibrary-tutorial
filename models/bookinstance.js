f7700fdbacf2801d09a2b0bdf43c2799c3b43f19
James Olsen <olsenjames1116@gmail.com>
2023-12-02 21:03:57 -0500

    Create the genre model

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const BookInstance = new Schema({
+const BookInstanceSchema = new Schema({
 	book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
 	imprint: { type: String, required: true },
 	status: {
