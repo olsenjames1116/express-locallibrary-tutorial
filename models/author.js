331fbb4cb59ab325fbc4e2ad4c9b190c4b2c65e0
James Olsen <olsenjames1116@gmail.com>
2023-12-04 18:57:25 -0500

    Display formatted death date in author_list view

--- models/author.js
+++ models/author.js
@@ -28,10 +28,18 @@ AuthorSchema.virtual('url').get(function () {
 	return `/catalog/author/${this._id}`;
 });
 
+//Virtual for author's birth date formatted.
 AuthorSchema.virtual('date_of_birth_formatted').get(function () {
 	return this.date_of_birth
 		? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
-		: '';
+		: 'N/A';
+});
+
+//Virtual for author's death date formatted.
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+	return this.date_of_death
+		? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+		: 'N/A';
 });
 
 //Export model
