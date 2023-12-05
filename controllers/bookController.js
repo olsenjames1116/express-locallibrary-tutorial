81618b56d28bfe55745fc841da5417c274cc37f5
James Olsen <olsenjames1116@gmail.com>
2023-12-05 18:00:31 -0500

    Add get route for update book controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -162,9 +162,36 @@ exports.book_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display book update form on GET.
 exports.book_update_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Book update GET');
-});
+	// Get book, authors and genres for form.
+	const [book, allAuthors, allGenres] = await Promise.all([
+		Book.findById(req.params.id).populate('author').populate('genre').exec(),
+		Author.find().sort({ family_name: 1 }).exec(),
+		Genre.find().sort({ name: 1 }).exec(),
+	]);
 
+	if (book === null) {
+		// No results.
+		const err = new Error('Book not found');
+		err.status = 404;
+		return next(err);
+	}
+
+	// Mark our selected genres as checked.
+	for (const genre of allGenres) {
+		for (const book_g of book.genre) {
+			if (genre._id.toString() === book_g._id.toString()) {
+				genre.checked = 'true';
+			}
+		}
+	}
+
+	res.render('book_form', {
+		title: 'Update Book',
+		authors: allAuthors,
+		genres: allGenres,
+		book: book,
+	});
+});
 // Handle book update on POST.
 exports.book_update_post = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Book update POST');
