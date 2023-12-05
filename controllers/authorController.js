9bca6270d0e54d9fe19f6f88328daff2224e6679
James Olsen <olsenjames1116@gmail.com>
2023-12-05 12:07:56 -0500

    Route to form for create author controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author');
 const asyncHandler = require('express-async-handler');
 const Book = require('../models/book');
+const { body, validationResult } = require('express-validator');
 
 //Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -36,7 +37,7 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
 
 //Display Author create form on GET.
 exports.author_create_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Author create GET');
+	res.render('author_form', { title: 'Create Author' });
 });
 
 //Handle Author create on POST/
