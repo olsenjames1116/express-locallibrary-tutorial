3801dc328d44d1c4e61205e42086f829123879c2
James Olsen <olsenjames1116@gmail.com>
2023-12-05 22:02:52 -0500

    Add post method to delete genre from the database

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -103,7 +103,26 @@ exports.genre_delete_get = asyncHandler(async (req, res, next) => {
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Genre delete POST');
+	// Get details of genres and all their books (in parallel).
+	const [genre, allBooksByGenre] = await Promise.all([
+		Genre.findById(req.params.id).exec(),
+		Book.find({ genre: req.params.id }, 'title summary').exec(),
+	]);
+
+	if (allBooksByGenre.length > 0) {
+		// Genre has books. Render in the same way as for GET route.
+		res.render('genre_delete'),
+			{
+				title: 'Delete Genre',
+				genre: genre,
+				genre_books: allBooksByGenre,
+			};
+		return;
+	} else {
+		// Genre has no books. Delete object and redirect to the list of genres.
+		await Genre.findByIdAndDelete(req.body.genreid);
+		res.redirect('/catalog/genres');
+	}
 });
 
 // Display Genre update form on GET.
