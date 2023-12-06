b945cf399372e660770acb558d7eb6c4bcb893d7
James Olsen <olsenjames1116@gmail.com>
2023-12-05 20:14:00 -0500

    Replace router get with post for update book
    An error was encountered when routing to the post method of the update
    book route. This error was resolved.

--- routes/catalog.js
+++ routes/catalog.js
@@ -28,7 +28,7 @@ router.post('/book/:id/delete', book_controller.book_delete_post);
 router.get('/book/:id/update', book_controller.book_update_get);
 
 // POST request to update Book.
-router.get('/book/:id/update', book_controller.book_update_post);
+router.post('/book/:id/update', book_controller.book_update_post);
 
 // GET request for one Book.
 router.get('/book/:id', book_controller.book_detail);
