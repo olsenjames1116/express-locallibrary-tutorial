01cba8f77206563ff6abcc3a203d57bb2265c70d
James Olsen <olsenjames1116@gmail.com>
2023-12-05 20:49:40 -0500

    Display and control get book delete route

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -152,7 +152,22 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Book delete GET');
+	// Get details of book and all their instances.
+	const [book, allBookInstances] = await Promise.all([
+		Book.findById(req.params.id).exec(),
+		BookInstance.find({ book: req.params.id }).exec(),
+	]);
+
+	if (book === null) {
+		// No results.
+		res.redirect('/catalog/books');
+	}
+
+	res.render('book_delete', {
+		title: 'Delete Book',
+		book: book,
+		book_instances: allBookInstances,
+	});
 });
 
 // Handle book delete on POST.
